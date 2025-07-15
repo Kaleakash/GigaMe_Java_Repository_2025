b2010a063681d5eb6766ba52bedc062d244d3cf7
Akash <akash300383@gmail.com>
2025-07-15 21:41:18 +0530

    initial commit

---
+++ Java Workspace/JavaOOPsExample/src/com/Car.java
@@ -0,0 +1,21 @@
+package com;
+
+public class Car {
+
+	String colour;
+	float price;
+	int wheel;
+	
+	void start() {
+		System.out.println("Car Start");
+	}
+	void appliedGear() {
+		System.out.println("Aplied Gear");
+	}
+	void moving() {
+		System.out.println("Car is moving");
+	}
+	void stop() {
+		System.out.println("Car Stop");
+	}
+}
---
+++ Java Workspace/JavaOOPsExample/src/com/CarTest.java
@@ -0,0 +1,16 @@
+package com;
+
+public class CarTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//start();
+		Car bmw = new Car();	// created memory 
+		Car innova = new Car();
+		Car santro = new Car();
+		bmw.start();
+		bmw.stop();
+		
+	}
+
+}
